b1fe43a7db3ad6b74daae802bd9249aa84e48863
Daniel <demartinezgatell@gmail.com>
2019-11-18 15:04:36 +0000

    topping up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,16 @@
 class Oystercard
-  # attr_reader :balance
+   attr_reader :balance
 
   DEFAULT_BALANCE = 0
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
-  
+
+  def top_up(amount)
+    @balance += amount
+  end 
+
+
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,8 +3,15 @@ require 'oystercard'
 describe Oystercard do
 
   describe "initialize" do
-    it "should have an initial balance of 0" do 
+    it "should have an initial balance of 0" do
       expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe "#top_up" do
+    it "Should top up card with balance passed in as argument" do
+      ORIGINAL = subject.balance
+      expect{subject.top_up(5)}. to change {subject.balance} .by(5)
     end
   end
 
