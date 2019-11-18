6b90ac691d7652dcfaee2d72b490229be50b6681
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-18 15:30:32 +0000

    balance limit enforced

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,15 +2,15 @@ class Oystercard
    attr_reader :balance
 
   DEFAULT_BALANCE = 0
+  DEFAULT_LIMIT = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
   def top_up(amount)
+    raise RuntimeError, "Balance cannot exceed £#{DEFAULT_LIMIT}" if (amount + @balance) > DEFAULT_LIMIT
     @balance += amount
   end 
-
-
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,6 +13,10 @@ describe Oystercard do
       ORIGINAL = subject.balance
       expect{subject.top_up(5)}. to change {subject.balance} .by(5)
     end
-  end
 
+    it "should not allow to top up to more than 90" do
+      topup = Oystercard::DEFAULT_LIMIT + 1
+      expect { subject.top_up(topup) }.to raise_error RuntimeError
+    end
+  end
 end
